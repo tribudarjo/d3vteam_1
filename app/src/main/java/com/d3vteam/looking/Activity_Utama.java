d392a6c0e73896f88d46856f923d5df94c758a4e
TRISATYO <tribudarjo@gmail.com>
2017-04-24 11:01:28 +0700

    commit master

---
+++ app/src/main/java/com/d3vteam/looking/Activity_Utama.java
@@ -0,0 +1,13 @@
+package com.d3vteam.looking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Utama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__utama);
+    }
+}
