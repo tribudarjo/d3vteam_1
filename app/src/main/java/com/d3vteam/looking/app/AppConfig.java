63ba76d2dac59fa3f49788e1482afb8c2fa11de7
Alienware\Macbook <syahbanaendraprakoso2@gmail.com>
2017-05-22 19:23:22 +0700

    Proses Login

---
+++ app/src/main/java/com/d3vteam/looking/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.d3vteam.looking.app;
+
+/**
+ * Created by Macbook on 5/22/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://looking.000webhostapp.com/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://looking.000webhostapp.com/android_login_api/register.php";
+}
