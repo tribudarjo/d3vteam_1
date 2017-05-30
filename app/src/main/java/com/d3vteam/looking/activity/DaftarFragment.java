2bba2888a4c540d7668bfecbbdacc3d1175c3ec8
Alienware\Macbook <syahbanaendraprakoso2@gmail.com>
2017-05-30 22:06:02 +0700

    test

---
+++ app/src/main/java/com/d3vteam/looking/activity/DaftarFragment.java
@@ -0,0 +1,30 @@
+package com.d3vteam.looking.activity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.d3vteam.looking.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DaftarFragment extends Fragment {
+
+
+    public DaftarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_daftar, container, false);
+    }
+
+}
